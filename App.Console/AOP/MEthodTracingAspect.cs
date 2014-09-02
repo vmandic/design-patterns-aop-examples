0d283f3bb4f36448ea4478f421369976bf864518
Vedran Mandić <vedran.mandic@mobendo.com>
2014-09-02 03:10:54 +0200

    Project finished. Added PostSharp AOP and UnitTesting.

---
+++ App.Console/AOP/MEthodTracingAspect.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+using System.Diagnostics;
+
+namespace App.ConsoleApp.AOP
+{
+    [Serializable]
+    public class TraceStartEndAttribute : OnMethodBoundaryAspect
+    {
+        private readonly string mCategory;
+        private readonly bool mMeassureTime;
+        private DateTime mStopwatchStart;
+        private DateTime mStopwatchEnd;
+        public string Category { get { return mCategory; } }
+
+        /// <summary>
+        /// Constructor which keeps track if timing will be needed.
+        /// </summary>
+        /// <param name="meassureTime">A boolean to indicate if time tracking is activated.</param>
+        public TraceStartEndAttribute(bool meassureTime)
+        {
+            this.mMeassureTime = meassureTime;
+        }
+
+        /// <summary>
+        /// Defines the tracing category for the static Trace class.
+        /// </summary>
+        /// <param name="category">A trace category.</param>
+        public TraceStartEndAttribute(TracingType categoryEnum, bool meassureTime = false)
+            : this(meassureTime)
+        {
+            this.mCategory = categoryEnum.ToString();
+        }
+
+        /// <summary>
+        /// Defines the tracing category for the static Trace class.
+        /// </summary>
+        /// <param name="category">A custom trace category.</param>
+        public TraceStartEndAttribute(string customCategory, bool meassureTime = false)
+            : this(meassureTime)
+        {
+            this.mCategory = customCategory;
+        }
+
+        /// <summary>
+        /// Records the entry to a method.
+        /// </summary>
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (this.mMeassureTime)
+                this.mStopwatchStart = DateTime.Now;
+
+            Trace.WriteLine(String.Format("Entering {0}.{1}.", args.Method.DeclaringType.Name, args.Method.Name), this.mCategory);
+        }
+
+        /// <summary>
+        /// Records the exit from a method.
+        /// </summary>
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Trace.WriteLine(String.Format("Exiting {0}.{1}.", args.Method.DeclaringType.Name, args.Method.Name), this.mCategory);
+
+            if (this.mMeassureTime)
+            {
+                this.mStopwatchEnd = DateTime.Now;
+                Trace.WriteLine(String.Format("Time in miliseconds elapsed: {0}ms", this.GetElapsedTimeInMilliseconds()), TracingType.Timer.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Returns the timer elapsed time in milliseconds.
+        /// </summary>
+        /// <returns>Time passed during the meassuring in milliseconds.</returns>
+        private int GetElapsedTimeInMilliseconds()
+        {
+            return (this.mStopwatchEnd - this.mStopwatchStart).Milliseconds;
+        }
+    }
+}
--- App.Console/Program.cs
+++ App.Console/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using App.ConsoleApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App.Console
+namespace App.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Creating a person...");
+            Person mPerson = new Person("Ivo", "Ivanek", 24);
+            Console.WriteLine(String.Format("Person {0} created!", mPerson.Fullname));
+
+            mPerson.SelfIntroduce();
+            mPerson.Say("Hi!!! Whats up?\nDo you like learning AOP?");
+            mPerson.Say("Now I'm going to give a try in counting!");
+            mPerson.CountFastToADesiredNumber(2000);
+
+            Console.WriteLine("Read the output/debug window in Visual Studio now! :)");
+            Console.ReadLine();
         }
     }
 }
